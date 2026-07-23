146737afaff902087428fdb58dff92cb184e92fc
Akash Laha <akashlaha48@gmail.com>
2026-07-24 01:55:34 +0530

    fix

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -3,18 +3,8 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-let convex: ConvexReactClient | null = null;
-
-if (typeof process !== "undefined" && process.env.NEXT_PUBLIC_CONVEX_URL) {
-  convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
-}
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  if (!convex) {
-    throw new Error(
-      "NEXT_PUBLIC_CONVEX_URL is not defined. Please define it in your .env.local file. See https://convex.dev/docs/getting-started#1-connect-your-project"
-    );
-  }
-
   return <ConvexProvider client={convex}>{children}</ConvexProvider>;
 }
